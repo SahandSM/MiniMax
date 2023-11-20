eb6cc227cc2e819d3e8e0a00ba9826591fda0714
Sahand <sahand.sabahi@gmail.com>
2023-11-21 00:29:09 +0100

    rename

---
+++ tests/test_game_utils/test_all.py
@@ -0,0 +1,67 @@
+
+import numpy as np
+
+from agents.game_utils import *
+
+def test_action():
+       board = np.array([[1,1,1,1,1,1,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,0]])
+       board_original = apply_player_action(board,7,PLAYER1)
+       assert np.all(board - board_original == np.array([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,1]]))
+
+def test_connected_not():
+    board = np.array([[1,1,1,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,1,0,1,0,0],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+    player =1
+    assert connected_four(board,1) == False
+
+def test_connected_hor():
+    board = np.array([[1,1,1,1,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,1,0,1,0,0],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+    player =1
+    assert connected_four(board,1) == True
+
+def test_connected_ver():
+    board = np.array([[1,1,1,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,1,0,1,0,1],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+    player =1
+    assert connected_four(board,1) == True
+
+def test_connected_diagp():
+    board = np.array([[1,1,1,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,1,0,0,1],[0,0,1,0,0,0,0],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+    player =1
+    assert connected_four(board,1) == True
+
+def test_connected_diagn():
+    board = np.array([[1,1,1,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,1,0,0,1],[0,0,0,0,1,0,0],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+    player =1
+    assert connected_four(board,1) == True
+
+def test_draw():
+    displayed_state = []
+
+    def mock_display(Gstate):  
+        displayed_state.append(Gstate)
+
+    board = np.arange(1,43).reshape(6,7)
+
+    check_end_state(board,1,mock_display)
+    assert displayed_state == ['IS_DRAW']
+
+def test_win():
+    displayed_state = []
+
+    def mock_display(Gstate):  
+        displayed_state.append(Gstate)
+
+    board = np.array([[1,1,1,1,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,1,0,1,0,0],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+
+    check_end_state(board,1,mock_display)
+    assert displayed_state == ['IS_WIN']
+
+def test_still_playing():
+    displayed_state = []
+
+    def mock_display(Gstate):  
+        displayed_state.append(Gstate)
+
+    board = np.array([[1,1,1,0,0,0,1],[0,0,0,0,0,0,1],[0,0,0,0,0,0,1],[0,0,1,0,1,0,0],[0,1,0,0,0,1,0],[1,0,0,0,0,0,1]])
+
+    check_end_state(board,1,mock_display)
+    assert displayed_state == ['STILL_PLAYING']
