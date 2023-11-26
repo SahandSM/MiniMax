9948b7687b49d0742d6c322cfb52f3b6df48ea4b
Sahand <sahand.sabahi@gmail.com>
2023-11-26 19:25:13 +0100

    initialize heuristic

--- agents/agent_minimax/heuristic.py
+++ agents/agent_minimax/heuristic.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 def WinChance_horz(board, player):
+    pass
+
+def next_row(board):
+
+    free_row = [np.count_nonzero(board[:,i]) for i in range(7)]
+
+    return free_row
 
     
     
