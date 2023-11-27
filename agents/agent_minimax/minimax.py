ff3f239e229479cb9329ef4d158e08513dd77744
Sahand <sahand.sabahi@gmail.com>
2023-11-27 20:54:26 +0100

    deleted all code and moved new from jupyter

--- agents/agent_minimax/minimax.py
+++ agents/agent_minimax/minimax.py
@@ -1,38 +1,35 @@
 
 import numpy as np
 
-from agents.game_utils import apply_player_action, connected_four, BoardPiece, PlayerAction, SavedState, NO_PLAYER
+from agents.game_utils import *
 from typing import Optional, Callable
 
 
 def generate_move_minimax(board: np.ndarray, 
                          player: BoardPiece, 
                          saved_state: Optional[SavedState]) -> tuple[PlayerAction, Optional[SavedState]]:
-    valid_moves = get_valid_moves(board)
-
-    action, is_win = get_winning_move(board,player,valid_moves)
-    if not is_win:
-        print('choosing randomly: ',valid_moves)
-        action = np.random.choice(valid_moves)
-
-    return action, saved_state
-
-def get_winning_move(board:np.ndarray, player: BoardPiece, valid_moves: np.ndarray):
-    is_win = False
-    action = []
-    for column in valid_moves:
-        borad_inspect_move = board.copy()
-        borad_inspect_move = apply_player_action(borad_inspect_move, column, player)
-        print('checking win:', column)
-        is_win = connected_four(borad_inspect_move, player)
-        print('is win?', is_win)
-        if is_win:
-            action = column
-            return action , is_win
-    return action, is_win
+    # return action, saved_state
+    pass
 
 def get_valid_moves(board: np.ndarray):
-    is_open = board[-1, :] == NO_PLAYER
+    is_open = board[-1, :] == 0
     valid_moves = np.arange(board.shape[1])[is_open]
-    print('checking valid moves: ', valid_moves)
     return valid_moves
+
+def is_open_row(board):
+    is_open = board[-1, :] == 0
+    return is_open
+
+def get_free_row(board):
+    # remember to check for legal moves first.
+    free_row = [np.count_nonzero(board[:,i]) for i in range(7)]
+    return free_row
+
+
+def minimiax_move(board,depth):
+    board_copy = board.copy()
+
+    if depth>0:
+        board_copy = apply_player_action(board_copy,6,2)
+        minimiax_move(board_copy,depth-1)
+    return board_copy
