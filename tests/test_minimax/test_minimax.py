30712e26bae46819fd9f688d68c4cdccdb71f01f
Sahand <sahand.sabahi@gmail.com>
2023-11-28 23:50:31 +0100

    test get best move added

--- tests/test_minimax/test_minimax.py
+++ tests/test_minimax/test_minimax.py
@@ -33,9 +33,28 @@ def test_iterate_states():
     '''
     board = string_to_board(board_string)
     # board_copy = board.copy() it is not necessary if you are suare you are making a copy in the agent before applying actions there
-    iterate_states(board,2)
+    score = iterate_states(board,2)
     print('\nOriginal\n'
         ,pretty_print_board(board))
+    print('score is :',score)
+    return
+
+def test_get_best_move():
+    board_string = ''' 
+    - - - - - - - 
+    |             |
+    |             |
+    |             |
+    |             |
+    |             |
+    |             |
+    - - - - - - -
+    0 1 2 3 4 5 6
+    '''
+    board = string_to_board(board_string)
+    # board_copy = board.copy() it is not necessary if you are suare you are making a copy in the agent before applying actions there
+    best_move = get_best_move(board)
+    print('best columns to take is:',best_move)
     return
 
 
