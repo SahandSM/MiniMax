967d9716c3571f5195d3a1e7ade373702331afbe
Sahand <sahand.sabahi@gmail.com>
2023-11-28 23:49:17 +0100

    minimax evaluates best possible score for  board

--- agents/agent_minimax/minimax.py
+++ agents/agent_minimax/minimax.py
@@ -27,7 +27,7 @@ def get_free_row(board):
     return free_row
 
 
-def iterate_states(board,depth,player = True):
+def iterate_states_0(board,depth,player = True):
     # where to copy?
     # assing the return or not?
         # what to return?
@@ -46,3 +46,40 @@ def iterate_states(board,depth,player = True):
             new_board = apply_player_action(new_board,move,1)
             iterate_states(new_board,depth-1,True)
     
+def iterate_states(board,depth,player = True, i=np.array([0])):
+    # where to copy?
+    # assing the return or not?
+        # what to return?
+    if depth == 0: # depth =0 means there has been x=depth moves carried out
+        print(f'\ndepth 0 reached for {i}:\n',pretty_print_board(board))
+        board_score = evaluate(i)
+        i += 1
+        return board_score
+
+    if player == True:
+        max_score = -100
+        for move in range(2):
+            new_board = board.copy() # copy should be done inside the loop becasue ... don't move it out
+            new_board = apply_player_action(new_board,move,2)
+            board_score = iterate_states(new_board,depth-1,False,i)
+            max_score = max(max_score,board_score)
+        return max_score
+    else:
+        min_score = +100
+        for move in range(2):
+            new_board = board.copy() # copy should be done inside the loop becasue ... don't move it out
+            new_board = apply_player_action(new_board,move,1)
+            board_score = iterate_states(new_board,depth-1,True,i)
+            min_score = min(min_score,board_score)
+        return min_score
+
+def evaluate(i):
+    if i == [0]:
+        return 1
+    if i == [1]:
+        return 2
+    if i == [2]:
+        return 0
+    if i == [3]:
+        return -1
+
