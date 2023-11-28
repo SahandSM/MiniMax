d17afcfff6320180ec12a4a44baab9917129d8bd
Sahand <sahand.sabahi@gmail.com>
2023-11-28 15:26:36 +0100

    test print possible states with one move

--- tests/test_minimax/test_minimax.py
+++ tests/test_minimax/test_minimax.py
@@ -1,8 +1,10 @@
+import numpy as np
+
+from agents.game_utils import *
+from agents.agent_minimax.minimax import *
 
 
-
-
-def test_heuristic():
+def test_minimax_1():
     board_string = ''' 
     - - - - - - - 
     |             |
@@ -17,10 +19,26 @@ def test_heuristic():
     board = string_to_board(board_string)
     return
 
-def test_heuristic():
-    board_copy = minimiax_move(board,3)
-    print('Original'
+def test_iterate_states():
+    board_string = ''' 
+    - - - - - - - 
+    |             |
+    |             |
+    |             |
+    |             |
+    |             |
+    |             |
+    - - - - - - -
+    0 1 2 3 4 5 6
+    '''
+    board = string_to_board(board_string)
+    board_copy = board.copy()
+    depth0(board_copy,2)
+    print('\nOriginal\n'
         ,pretty_print_board(board))
-    print('Copy'
+    print('Copy\n'
         ,pretty_print_board(board_copy))
     return
+
+
+    
