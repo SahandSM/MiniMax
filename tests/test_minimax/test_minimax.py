7937325fb5ab97f6134bf56d2318d8fb05413a04
Sahand <sahand.sabahi@gmail.com>
2023-12-01 20:59:44 +0100

    rename open row to open col

--- tests/test_minimax/test_minimax.py
+++ tests/test_minimax/test_minimax.py
@@ -19,6 +19,40 @@ def test_minimax_1():
     board = string_to_board(board_string)
     return
 
+def test_is_open_col():
+    board_string = ''' 
+     - - - - - - - 
+    |        O    |
+    |        O    |
+    |        O    |
+    |O O   X X X  |
+    |X O   X O X  |
+    |X X O X O O X|
+     - - - - - - -
+    0 1 2 3 4 5 6
+    '''
+    board = string_to_board(board_string)
+    is_open = is_open_col(board)
+    print(is_open)
+    return
+
+def test_get_valid_moves():
+    board_string = ''' 
+     - - - - - - - 
+    |        O    |
+    |        O    |
+    |        O    |
+    |O O   X X X  |
+    |X O   X O X  |
+    |X X O X O O X|
+     - - - - - - -
+    0 1 2 3 4 5 6
+    '''
+    board = string_to_board(board_string)
+    valid_moves = get_valid_moves(board)
+    print(valid_moves)
+    return
+
 def test_iterate_states():
     board_string = ''' 
     - - - - - - - 
