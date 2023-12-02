8b486565ecd7a7a4cd6dfe2dd3387a1e1a254378
Sahand <sahand.sabahi@gmail.com>
2023-12-02 01:05:06 +0100

    print test for col, diag, opp_diag pivot()

--- tests/test_minimax/test_heuristic.py
+++ tests/test_minimax/test_heuristic.py
@@ -52,9 +52,48 @@ def test_pivot_row():
 
     print(board[pivot])
 
-    array_window, position = pivot_row(board,pivot)
-    print(array_window)
+    row_window, position = pivot_row(board,pivot)
+    print('row_to_extract:',row_window)
+    print('position:',position)
+
+
+def test_pivot_col():
+    board = np.arange(1,43).reshape(6,7)
+    print('\n',board)
+
+    pivot = (2,3)
+    print(pivot)
+
+    print(board[pivot])
+
+    col_window, position = pivot_col(board,pivot)
+    print('col_to_extract:',col_window)
+    print('position:',position)
+
+
+def test_pivot_diag():
+    board = np.arange(1,43).reshape(6,7)
+    print('\n',board)
+
+    pivot = (2,3)
+    print(pivot)
+
+    print(board[pivot])
+
+    diag_window, position = pivot_diag(board,pivot)
+    print('daig_to_extract:',diag_window)
+    print('position:',position)
+
+def test_pivot_opp_diag():
+    board = np.arange(1,43).reshape(6,7)
+    print('\n',board)
+
+    pivot = (2,3)
+    print(pivot)
+
+    print(board[pivot])
+
+    opp_daig_window, position = pivot_opp_diag(board,pivot)
+    print(opp_daig_window)
     print(position)
-
-
 
