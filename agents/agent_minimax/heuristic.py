646148bcc058e7a3d3bc9ab69694e0c45d4993db
Sahand <sahand.sabahi@gmail.com>
2023-12-02 14:11:53 +0100

    rearranged order of unctinos

--- agents/agent_minimax/heuristic.py
+++ agents/agent_minimax/heuristic.py
@@ -3,6 +3,65 @@ import numpy as np
 from agents.game_utils import *
 from typing import Optional, Callable
 
+def evaluate_at_pivot(board,pivot,player):
+    score = []
+    score_row = evaluate_row(board,pivot,player)
+    score_col = evaluate_col(board,pivot,player)
+    score_diag = evaluate_diag(board,pivot,player)
+    score_opp_diag = evaluate_opp_diag(board,pivot,player)
+    score = score_row + score_col + score_diag + score_opp_diag
+    return score
+
+def evaluate_row(board,pivot_point,player):
+    array,position = pivot_row(board,pivot_point)
+    windows_row = extract_windows(array,position)
+    score_row = evaluate_single_direction(windows_row,player)
+    return score_row
+
+def pivot_row(board,pivot):
+    """
+    return the row contraining the pivot.
+    """
+    row_window = board[pivot[0]]
+    position = pivot[1]
+    return row_window,position
+
+def evaluate_col(board,pivot_point,player):
+    array,position = pivot_col(board,pivot_point)
+    windows_col = extract_windows(array,position)
+    score_col = evaluate_single_direction(windows_col,player)
+    return score_col
+
+def pivot_col(board,pivot):
+    col_window = board[:,pivot[1]]
+    position = pivot[0]
+    return col_window,position
+
+def evaluate_diag(board,pivot_point,player):
+    array,position = pivot_diag(board,pivot_point)
+    windows_diag = extract_windows(array,position)
+    sccore_diag = evaluate_single_direction(windows_diag,player)
+    return sccore_diag
+
+def pivot_diag(board,pivot):
+    diag_window = np.diag(board,pivot[1]-pivot[0])
+    position = min(pivot)
+    return diag_window,position
+
+def evaluate_opp_diag(board,pivot_point,player):
+    array,position = pivot_opp_diag(board,pivot_point)
+    windows_opp_diag = extract_windows(array,position)
+    score_opp_diag = evaluate_single_direction(windows_opp_diag,player)
+    return score_opp_diag
+
+def pivot_opp_diag(board,pivot):
+    borad_flipped = np.fliplr(board)
+    pivot = pivot[0],6-pivot[1]
+    opp_daig_window = np.diag(borad_flipped,pivot[1]-pivot[0])
+    position = min(pivot)
+    return opp_daig_window,position
+
+
 def extract_windows(array: list,pivot_position: int) -> list:
     """
     Extracts a sliding window of size 4 from the given list 'array' centered around the 'pivot_position'.
@@ -28,64 +87,6 @@ def extract_windows(array: list,pivot_position: int) -> list:
         window = list(array[w:w+4])
         windows.append(window)
     return windows
-
-def pivot_row(board,pivot):
-    """
-    return the row contraining the pivot.
-    """
-    row_window = board[pivot[0]]
-    position = pivot[1]
-    return row_window,position
-
-def pivot_col(board,pivot):
-    col_window = board[:,pivot[1]]
-    position = pivot[0]
-    return col_window,position
-
-def pivot_diag(board,pivot):
-    diag_window = np.diag(board,pivot[1]-pivot[0])
-    position = min(pivot)
-    return diag_window,position
-
-def pivot_opp_diag(board,pivot):
-    borad_flipped = np.fliplr(board)
-    pivot = pivot[0],6-pivot[1]
-    opp_daig_window = np.diag(borad_flipped,pivot[1]-pivot[0])
-    position = min(pivot)
-    return opp_daig_window,position
-
-def evaluate_at_pivot(board,pivot,player):
-    score = []
-    score_row = evaluate_row(board,pivot,player)
-    score_col = evaluate_col(board,pivot,player)
-    score_diag = evaluate_diag(board,pivot,player)
-    score_opp_diag = evaluate_opp_diag(board,pivot,player)
-    score = score_row + score_col + score_diag + score_opp_diag
-    return score
-
-def evaluate_row(board,pivot_point,player):
-    array,position = pivot_row(board,pivot_point)
-    windows_row = extract_windows(array,position)
-    score_row = evaluate_single_direction(windows_row,player)
-    return score_row
-
-def evaluate_col(board,pivot_point,player):
-    array,position = pivot_col(board,pivot_point)
-    windows_col = extract_windows(array,position)
-    score_col = evaluate_single_direction(windows_col,player)
-    return score_col
-
-def evaluate_diag(board,pivot_point,player):
-    array,position = pivot_diag(board,pivot_point)
-    windows_diag = extract_windows(array,position)
-    sccore_diag = evaluate_single_direction(windows_diag,player)
-    return sccore_diag
-
-def evaluate_opp_diag(board,pivot_point,player):
-    array,position = pivot_opp_diag(board,pivot_point)
-    windows_opp_diag = extract_windows(array,position)
-    score_opp_diag = evaluate_single_direction(windows_opp_diag,player)
-    return score_opp_diag
 
 def evaluate_single_direction(windows,player):
     # I need to evlaute each direction separately to catch if for example therer is both horizontal and diagonal score 3.
