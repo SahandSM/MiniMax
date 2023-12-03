810ecf492ad7e1b4e598b18e88f87f50ab33ae40
Sahand <sahand.sabahi@gmail.com>
2023-12-03 22:04:10 +0100

    converted print tests to assert tests

--- tests/test_minimax/test_heuristic/test_pivot_axis.py
+++ tests/test_minimax/test_heuristic/test_pivot_axis.py
@@ -7,13 +7,16 @@ def test_pivot_row():
     print('\n',board)
 
     pivot = (2,3)
-    print(pivot)
+    print('pivot point:',pivot)
 
-    print(board[pivot])
+    print('pivot point value:',board[pivot])
 
     row_window, position = pivot_row(board,pivot)
     print('row_to_extract:',row_window)
     print('position:',position)
+    
+    assert position == 3
+    assert np.all(row_window == [15, 16, 17, 18, 19, 20, 21]) 
 
 
 def test_pivot_col():
@@ -29,6 +32,8 @@ def test_pivot_col():
     print('col_to_extract:',col_window)
     print('position:',position)
 
+    assert position == 2
+    assert np.all( col_window == [ 4, 11, 18, 25, 32, 39])
 
 def test_pivot_diag():
     board = np.arange(1,43).reshape(6,7)
@@ -43,6 +48,9 @@ def test_pivot_diag():
     print('daig_to_extract:',diag_window)
     print('position:',position)
 
+    assert np.all(diag_window == [ 2, 10, 18, 26, 34, 42])
+    assert position == 2
+
 def test_pivot_opp_diag():
     board = np.arange(1,43).reshape(6,7)
     print('\n',board)
@@ -55,3 +63,6 @@ def test_pivot_opp_diag():
     opp_daig_window, position = pivot_opp_diag(board,pivot)
     print(opp_daig_window)
     print(position)
+
+    assert np.all(opp_daig_window == [ 6, 12, 18, 24, 30, 36])
+    assert position == 2
