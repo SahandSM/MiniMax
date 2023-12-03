383d5e0b039718b4191e0dd022fb9d4f2915d2d1
Sahand <sahand.sabahi@gmail.com>
2023-12-03 23:23:39 +0100

    renamed file to test_get_pivot_axis

--- tests/test_minimax/test_heuristic/test_get_pivot_axis.py
+++ tests/test_minimax/test_heuristic/test_get_pivot_axis.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from agents.agent_minimax.heuristic import *
 
-def test_pivot_row():
+def test_get_pivot_row():
     board = np.arange(1,43).reshape(6,7)
     print('\n',board)
 
@@ -11,7 +11,7 @@ def test_pivot_row():
 
     print('pivot point value:',board[pivot])
 
-    row_window, position = pivot_row(board,pivot)
+    row_window, position = get_pivot_row(board,pivot)
     print('row_to_extract:',row_window)
     print('position:',position)
     
@@ -19,7 +19,7 @@ def test_pivot_row():
     assert np.all(row_window == [15, 16, 17, 18, 19, 20, 21]) 
 
 
-def test_pivot_col():
+def test_get_pivot_col():
     board = np.arange(1,43).reshape(6,7)
     print('\n',board)
 
@@ -28,14 +28,14 @@ def test_pivot_col():
 
     print(board[pivot])
 
-    col_window, position = pivot_col(board,pivot)
+    col_window, position = get_pivot_col(board,pivot)
     print('col_to_extract:',col_window)
     print('position:',position)
 
     assert position == 2
     assert np.all( col_window == [ 4, 11, 18, 25, 32, 39])
 
-def test_pivot_diag():
+def test_get_pivot_diag():
     board = np.arange(1,43).reshape(6,7)
     print('\n',board)
 
@@ -44,14 +44,14 @@ def test_pivot_diag():
 
     print(board[pivot])
 
-    diag_window, position = pivot_diag(board,pivot)
+    diag_window, position = get_pivot_diag(board,pivot)
     print('daig_to_extract:',diag_window)
     print('position:',position)
 
     assert np.all(diag_window == [ 2, 10, 18, 26, 34, 42])
     assert position == 2
 
-def test_pivot_opp_diag():
+def test_get_pivot_opp_diag():
     board = np.arange(1,43).reshape(6,7)
     print('\n',board)
 
@@ -60,7 +60,7 @@ def test_pivot_opp_diag():
 
     print(board[pivot])
 
-    opp_daig_window, position = pivot_opp_diag(board,pivot)
+    opp_daig_window, position = get_pivot_opp_diag(board,pivot)
     print(opp_daig_window)
     print(position)
 
