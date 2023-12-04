f78e033e6344f48409f48573e07baf1f056ea9c5
Sahand <sahand.sabahi@gmail.com>
2023-12-04 10:42:31 +0100

    Revert "removed unused argument player"
    This reverts commit 42e41e1443a88b8c12ac9d4f03ea7b811a0f681f.

--- agents/agent_random/random.py
+++ agents/agent_random/random.py
@@ -1,11 +1,12 @@
 
 import numpy as np
 
-from agents.game_utils import PlayerAction, SavedState, NO_PLAYER
+from agents.game_utils import BoardPiece, PlayerAction, SavedState, NO_PLAYER
 from typing import Optional, Callable
 
 
 def generate_move_random(board: np.ndarray, 
+                         player: BoardPiece, 
                          saved_state: Optional[SavedState]) -> tuple[PlayerAction, Optional[SavedState]]:
     # Choose a valid, non-full column randomly and return it as `action`
     valid_moves = get_valid_moves(board)
