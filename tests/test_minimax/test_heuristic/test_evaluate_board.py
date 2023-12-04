69415e0b3702d171ee7546779ab2c8f821bf209e
Sahand <sahand.sabahi@gmail.com>
2023-12-04 10:01:39 +0100

    mofidied comment

--- tests/test_minimax/test_heuristic/test_evaluate_board.py
+++ tests/test_minimax/test_heuristic/test_evaluate_board.py
@@ -5,14 +5,15 @@ from agents.agent_minimax.heuristic import *
 def test_evaluate_board():
     '''
     notes for later improvement:
-    1- this board is an example that shows the benefit of evaluating each axis separately.
-    such that we can check that for example X can reach a tow-way-win by placing a piece on (3,4).
+    1- this board is an example that shows the benefit of evaluating each axis separately and returning
+    acores of each pivot in four arrays of length four.
+    this helps to check for example that X can reach a tow-way-win by placing a piece on (3,4).
     although I have not implement this scenario in score aggregate function (due to lack of time),
     it shows the reason why the evalutation algorithem is designed this way.
     2- another point is, although player O doesn't have an immediate two-way-win state, the agent
-    avoids this board becasue O has two connect three on the board. this should be improved by checking
+    avoids this board becasue O has two connected threes on the board. this should be improved by checking
     the immediate pivots first and weight them differently. (not done yet due to time limit)
-    summary: the agent should weight the X position better than O. but it doesn't due to design of
+    summary: it is rational to weight the X position better than O. but the agent doesn't due to design of
     evalutation algorithem. However, the algorithem is working as expected.
     '''
     board_string = ''' 
